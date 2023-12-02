1802c403c43cfb0cb139164ea230a1680548bea3
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-12-01 22:55:25 -0500

    feat: added home component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./public/pages/home/home.component";
+import {MainPageComponent} from "./store/pages/main-page/main-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"store", component: MainPageComponent},
+];
---
+++ src/app/public/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ButtonModule} from "primeng/button";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ButtonModule, RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
